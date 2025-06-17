9f952acfac94ec2438023b2fea5abbabc3398c94
ilmvamehnat <hamzamamutov74@gmail.com>
2025-06-17 20:16:43 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
-app.use(express.json());
-app.post('/save', (req, res) => {
-  const data = JSON.stringify(req.body);
-  fs.writeFileSync('applications.json', data);
-  res.send({ status: 'ok' });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    let fileData = [];
+    try {
+        fileData = JSON.parse(fs.readFileSync('applications.json'));
+    } catch {}
+    fileData.push(data);
+    fs.writeFileSync('applications.json', JSON.stringify(fileData, null, 2));
+    res.redirect('/thanks.html');
 });
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
